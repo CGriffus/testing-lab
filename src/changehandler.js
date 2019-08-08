2d8a1803195adbb77aedf7ed64edc65b78b7a3e6
Caitlin <caitlin@griffus.com>
2019-08-08 17:12:13 -0400

    all tests passed

--- src/changehandler.js
+++ src/changehandler.js
@@ -14,23 +14,55 @@ class ChangeHandler {
    * The parameter "type" is a string that is either quarter, dime, nickel, or penny
    */
   insertCoin(typeOfCoin) {
-
+    if (typeOfCoin === "penny") {
+      this.cashTendered += 1;
+    } else if (typeOfCoin === "nickel") {
+      this.cashTendered += 5;
+    } else if (typeOfCoin === "dime") {
+      this.cashTendered += 10;
+    } else if (typeOfCoin === "quarter") {
+      this.cashTendered += 25;
+    }
   }
 
   /**
    * Returns true if enough coins have been inserted to at least meet the amountDue
    */
   isPaymentSufficient() {
-      
+    if (this.cashTendered < this.amountDue) {
+      return false;
+    } else if (this.cashTendered >= this.amountDue) {
+      return true;
+    }
   }
 
   giveChange() {
-    // TODO return the correct change in the following format...
+    let quarters = 0;
+    let dimes = 0;
+    let nickels = 0;
+    let pennies = 0;
+    let changeLeft = this.cashTendered - this.amountDue;
+
+    while (changeLeft > 0) {
+      if (changeLeft >= 25) {
+        changeLeft -= 25;
+        quarters++;
+      } else if (changeLeft >= 10) {
+        changeLeft -= 10;
+        dimes++;
+      } else if (changeLeft >= 5) {
+        changeLeft -= 5;
+        nickels++;
+      } else {
+        changeLeft -= 1;
+        pennies++;
+      }
+    }
     return {
-      quarters: 0,
-      dimes: 0,
-      nickels: 0,
-      pennies: 0
+      quarters: quarters,
+      dimes: dimes,
+      nickels: nickels,
+      pennies: pennies
     };
   }
 }
--- tests/changehandler.test.js
+++ tests/changehandler.test.js
@@ -1,11 +1,123 @@
 let { ChangeHandler } = require("../src/changehandler");
 
 describe("Tests for ChangeHandler", function() {
-
-    // Set up a test below...
-    test("The ChangeHandler class is defined.", function() {
-        // Remember, you can arrange, act, and assert...start small
-        expect(ChangeHandler).toBeDefined();
+  // Set up a test below...
+  test("The ChangeHandler class is defined.", function() {
+    // Remember, you can arrange, act, and assert...start small
+    expect(ChangeHandler).toBeDefined();
+  });
+  test("the amount due is set based on an argument", function() {
+    let vendingMachine = new ChangeHandler(100);
+    expect(vendingMachine.amountDue).toBe(100);
+  });
+  test("cashTendered is set to zero", function() {
+    let cash = new ChangeHandler(100);
+    expect(cash.cashTendered).toBe(0);
+  });
+  test("Inserting a penny adds 1 to cashTendered", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("penny");
+    expect(vendingMachine.cashTendered).toBe(1);
+  });
+  test("Inserting a nickel adds 5 to cashTendered", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("nickel");
+    expect(vendingMachine.cashTendered).toBe(5);
+  });
+  test("Inserting a dime adds 10 to cashTendered", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("dime");
+    expect(vendingMachine.cashTendered).toBe(10);
+  });
+  test("Inserting a quarter adds 25 to cashTendered", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("quarter");
+    expect(vendingMachine.cashTendered).toBe(25);
+  });
+  test("Calling function multiple times continues to add on to the amount", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("nickel");
+    vendingMachine.insertCoin("nickel");
+    vendingMachine.insertCoin("dime");
+    vendingMachine.insertCoin("quarter");
+    expect(vendingMachine.cashTendered).toBe(47);
+  });
+  test("Returns true if cashTendered more than amountDue", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    expect(vendingMachine.isPaymentSufficient()).toBe(true);
+  });
+  test("Returns false if cashTendered less than amountDue", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    expect(vendingMachine.isPaymentSufficient()).toBe(false);
+  });
+  test("Returns true if cashTendered equal to amountDue", function() {
+    const vendingMachine = new ChangeHandler(100);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    expect(vendingMachine.isPaymentSufficient()).toBe(true);
+  });
+  test("32 change produces: quarters: 1, dimes: 0, nickels: 1, pennies: 2", function() {
+    const vendingMachine = new ChangeHandler(5);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("nickel");
+    vendingMachine.insertCoin("nickel");
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("penny");
+    expect(vendingMachine.giveChange()).toEqual({
+      quarters: 1,
+      dimes: 0,
+      nickels: 1,
+      pennies: 2
     });
-
+  });
+  test("10 change produces: quarters: 0, dimes: 1, nickels: 0, pennies: 0", function() {
+    const vendingMachine = new ChangeHandler(0);
+    vendingMachine.insertCoin("dime");
+    expect(vendingMachine.giveChange()).toEqual({
+      quarters: 0,
+      dimes: 1,
+      nickels: 0,
+      pennies: 0
+    });
+  });
+  test("27 change produces: quarters: 1, dimes: 0, nickels: 0, pennies: 2", function() {
+    const vendingMachine = new ChangeHandler(0);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("penny");
+    expect(vendingMachine.giveChange()).toEqual({
+      quarters: 1,
+      dimes: 0,
+      nickels: 0,
+      pennies: 2
+    });
+  });
+  test("68 change produces: quarters: 2, dimes: 1, nickels: 1, pennies: 3", function() {
+    const vendingMachine = new ChangeHandler(0);
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("quarter");
+    vendingMachine.insertCoin("dime");
+    vendingMachine.insertCoin("nickel");
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("penny");
+    vendingMachine.insertCoin("penny");
+    expect(vendingMachine.giveChange()).toEqual({
+      quarters: 2,
+      dimes: 1,
+      nickels: 1,
+      pennies: 3
+    });
+  });
 });
